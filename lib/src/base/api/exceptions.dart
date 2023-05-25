4937c5bb219a44a34a36de56f519c3846aa5ce76
Everton Mantena Pereira <consultor5a.everton@webmotors.com.br>
2023-05-25 08:20:36 -0300

    removendo pasta base

--- lib/src/base/api/exceptions.dart
+++
@@ -1,5 +0,0 @@
-abstract class Exceptions {
-  static String socketExceptionMessage = "No Internet connection 😑";
-  static String httpException = "Couldn't find the path 😱";
-  static String formatException = "Bad response format 👎";
-}
